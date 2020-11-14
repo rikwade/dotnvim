2f8122a1fba83c2a01a44c31250f6565de61f37f
s1n7ax <srineshnisala@gmail.com>
2020-11-14 10:02:18 +0530

    [ADD][FEATURE] add key binds

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -3,12 +3,33 @@ local Keybind = require('vim.keybind')
 local KeybindType = Keybind.KeybindType
 local add_keybinds = Keybind.add_keybinds
 
-local buffer = {
-    -- [ j + k ] to go to normal mode from insert
-    { '', '<F6>', ':setlocal spell! spelllang=en_us<CR>' },
+local n_global = {
+    -- [ ctrl + h ] move cursor to left window
+    { 'n', '<c-h>', '<c-w>h' },
+
+    -- [ ctrl + l ] move cursor to right window
+    { 'n', '<c-l>', '<c-w>l' },
+
+    -- [ ctrl + j ] move cursor to bottom window
+    { 'n', '<c-j>', '<c-w>j' },
+
+    -- [ ctrl + k ] move cursor to top window
+    { 'n', '<c-k>', '<c-w>k' },
+
+    -- reload nvim configuration file
+    { 'n', ',r', ':so $MYVIMRC<cr>' },
+
+    -- edit nvim configuration file
+    { 'n', ',e', ':vsp $MYVIMRC<cr>' },
+
+    -- [ ctrl + s ] save the current buffer
+    { 'n', '<c-s>', ':w<cr>' },
+
+    -- [ ctrl + q ] quite the current buffer
+    { 'n', '<c-q>', ':q<cr>' },
 }
 
-local i_buffer = {
+local i_global = {
     -- [ j + k ] to go to normal mode from insert
     { 'i', 'jk', '<esc>' },
 
@@ -23,28 +44,17 @@ local i_buffer = {
 
     -- [ ctrl + q ] quite the current buffer
     { 'i', '<c-q>', '<esc>:q<cr>' },
-}
 
-local n_buffer = {
-    -- reload nvim configuration file
-    { 'n', ',r', ':so $MYVIMRC<cr>' },
-
-    -- edit nvim configuration file
-    { 'n', ',e', ':vsp $MYVIMRC<cr>' },
-
-    -- [ ctrl + s ] save the current buffer
-    { 'n', '<c-s>', ':w<cr>' },
-
-    -- [ ctrl + q ] quite the current buffer
-    { 'n', '<c-q>', ':q<cr>' },
+    -- [ <F6> ] to go enable spell checker
+    { '', '<F6>', ':setlocal spell! spelllang=en_us<CR>' },
 }
 
 local t_buffer = {
     -- [ j + k ] to go to normal mode from terminal
-    { 't', 'jk', [[ <C-\><C-n> ]] },
+    { 0, 't', 'jk', [[ <C-\><C-n> ]] },
 }
 
-add_keybinds(KeybindType.BUFFER_MAPPING, buffer)
-add_keybinds(KeybindType.BUFFER_MAPPING, i_buffer)
-add_keybinds(KeybindType.BUFFER_MAPPING, n_buffer)
+add_keybinds(KeybindType.GLOBAL_MAPPING, n_global)
+add_keybinds(KeybindType.GLOBAL_MAPPING, i_global)
+
 add_keybinds(KeybindType.BUFFER_MAPPING, t_buffer)
