ad12c9f3e9eb96493597ea77f7e68d5a07453e0d
s1n7ax <srineshnisala@gmail.com>
2020-11-14 10:03:35 +0530

    [FIX][FEATURE] fix add key bind issues
    * previously wrong parameters were passed in to bind API
    * fixed parameters

--- lua/vim/keybind.lua
+++ lua/vim/keybind.lua
@@ -1,37 +1,50 @@
-local KeyBind = {
-    KeybindType = {
-        GLOBAL_MAPPING = 'gm',
-        BUFFER_MAPPING = 'bm'
-    },
+local KeybindType = {
+    GLOBAL_MAPPING = 'gm',
+    BUFFER_MAPPING = 'bm'
+}
 
+local Keybind = {
+    KeybindType = KeybindType,
     add_keybinds = function (keybind_type, keybinds)
         if type(keybinds) ~= 'table' then
             error('keybinds should be a type of "table"')
             return
         end
 
-        local bind_type
-        if keybind_type == 'gm' then 
-            bind_type = vim.api.nvim_set_keymap
-        elseif keybind_type == 'bm' then
-            bind_type = vim.api.nvim_buf_set_keymap
-        end
+        -- global mapping
+        if keybind_type == KeybindType.GLOBAL_MAPPING then
 
-        for index, keybind in pairs(keybinds) do
+            for index, keybind in pairs(keybinds) do
+                if(keybind[4] == nil) then
+                    keybind[4] = {}
+                end
 
-            if(keybind[4] == nil) then
-                keybind[4] = {}
+                vim.api.nvim_set_keymap(
+                    keybind[1],
+                    keybind[2],
+                    keybind[3],
+                    keybind[4]
+                )
             end
 
-            bind_type(
-                keybind[0],
-                keybind[1],
-                keybind[2],
-                keybind[3],
-                keybind[4]
-            )
+        -- buffer mapping
+        elseif keybind_type == KeybindType.BUFFER_MAPPING then
+
+            for index, keybind in pairs(keybinds) do
+                if(keybind[5] == nil) then
+                    keybind[5] = {}
+                end
+                vim.api.nvim_buf_set_keymap(
+                    keybind[1],
+                    keybind[2],
+                    keybind[3],
+                    keybind[4],
+                    keybind[5]
+                )
+            end
         end
     end
 }
 
-return KeyBind
+
+return Keybind
