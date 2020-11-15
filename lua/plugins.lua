e19cf983bfa6596e04fc9d3c34b482e5bcd77f00
s1n7ax <srineshnisala@gmail.com>
2020-11-16 01:36:20 +0530

    [ADD][PKG] add plugins list

---
+++ lua/plugins.lua
@@ -0,0 +1,12 @@
+local packman = require('packman_lua.packman')
+
+packman.get('kyazdani42/nvim-tree.lua')
+packman.get('kyazdani42/nvim-web-devicons')
+packman.get('mhartington/oceanic-next')
+packman.get('neovim/nvim-lspconfig')
+packman.get('nvim-lua/completion-nvim')
+packman.get('junegunn/fzf')
+packman.get('junegunn/fzf.vim')
+-- diagnostic-nvim is no longer needed
+-- diagnostic features come with nvim core vim.lsp.diagnostic
+-- packman.get('nvim-lua/diagnostic-nvim')
