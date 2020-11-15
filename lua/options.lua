32f3fd8dd6636b794a9bc5a9d9932edd7e4b708f
s1n7ax <srineshnisala@gmail.com>
2020-11-16 01:28:07 +0530

    [ADD][FEATURE] add options

--- lua/options.lua
+++ lua/options.lua
@@ -3,10 +3,29 @@ local Option = require('vim.option')
 local OptionType = Option.OptionType;
 local add_options = Option.add_options;
 
+local global_options = {
+    shiftwidth = 4,
+    tabstop = 4,
+    foldmethod = 'syntax',
+	mouse = 'n',
+	completeopt = 'menuone,noinsert,noselect',
+	smartcase = true,
+	ignorecase = true,
+	wrap = false,
+	background = 'dark',
+	termguicolors = true,
+	inccommand = 'split',
+}
+
 local window_options = {
     number = true,
     relativenumber = true,
 }
 
+local buffer_options = {
+}
+
 -- adding options
+add_options(OptionType.GLOBAL_OPTION, global_options)
 add_options(OptionType.WINDOW_OPTION, window_options)
+add_options(OptionType.BUFFER_OPTION, buffer_options)
