6619448e34bf210ce6da45eb15282e4d353ff221
s1n7ax <srineshnisala@gmail.com>
2020-11-16 01:36:56 +0530

    [UPD][FEATURE] load config from init

--- lua/init.lua
+++ lua/init.lua
@@ -1,5 +1,15 @@
+-- basic configurations
 require('options')
 require('variables')
 require('keybinds')
+
+-- load package manager
 require('packman_lua.packman')
+
+-- plugin configurations
 require('lua-tree')
+require('fzf')
+require('completion-nvim')
+
+-- lsp
+require('lsp')
