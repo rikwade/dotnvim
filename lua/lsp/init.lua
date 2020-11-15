01b434d1b5e78dab9bd5818ca2a1d3efeecc3092
s1n7ax <srineshnisala@gmail.com>
2020-11-16 01:36:02 +0530

    [ADD][FEATURE] add lsp config

---
+++ lua/lsp/init.lua
@@ -0,0 +1,30 @@
+local nvim_lsp = require('nvim_lsp')
+local completion = require('completion')
+local Keybind = require('vim.keybind')
+
+local on_attach_callback = function(_, bufnr)
+    completion.on_attach()
+
+    local opts = { noremap = true }
+    local binds = {
+        { bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts },
+        { bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts },
+        { bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts },
+        { bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts },
+        { bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts },
+        { bufnr, 'n', '<leader>xD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts },
+        { bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts },
+        { bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts },
+        { bufnr, 'n', '<leader>o', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts },
+    }
+
+    Keybind.add_keybinds(Keybind.KeybindType.BUFFER_MAPPING, binds)
+end
+
+local servers = { 'jdtls', 'sumneko_lua' }
+
+for _, lsp in ipairs(servers) do
+    nvim_lsp [lsp].setup {
+        on_attach = on_attach_callback,
+    }
+end
