dc36e7eb451fbb919fcc9ed3f3e59f6882ab6f67
s1n7ax <srineshnisala@gmail.com>
2020-11-21 00:22:53 +0530

    [ADD] add fugitive key binds
    * add key binds for commit and diffput

--- lua/fugitive/init.lua
+++ lua/fugitive/init.lua
@@ -5,7 +5,8 @@ local add_keybinds = Keybind.add_keybinds
 
 local keybinds = {
 	{ 'n', '<leader>gs', '<cmd>Gstatus<cr>' },
+	{ 'n', '<leader>gc', '<cmd>Gcommit<cr>' },
+	{ 'n', '<leader>gpu', '<cmd>diffput<cr>' },
 }
 
 add_keybinds(KeybindType.GLOBAL_MAPPING, keybinds)
-
