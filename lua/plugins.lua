8fb1a10af2d70b3cfed9af2949b72e96dc8fe408
s1n7ax <srineshnisala@gmail.com>
2020-11-21 00:16:43 +0530

    [UPD][DOC] update comments

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,10 +1,21 @@
 local packman = require('packman_lua.packman')
 
+-- file explorer
 packman.get('kyazdani42/nvim-tree.lua')
+
+-- icons for nvim-tree file explorer
 packman.get('kyazdani42/nvim-web-devicons')
+
+-- color theme
 packman.get('mhartington/oceanic-next')
+
+-- language server configurations
 packman.get('neovim/nvim-lspconfig')
+
+-- auto completion features for lsp
 packman.get('nvim-lua/completion-nvim')
+
+-- fuzzy search features
 packman.get('junegunn/fzf')
 packman.get('junegunn/fzf.vim')
 
