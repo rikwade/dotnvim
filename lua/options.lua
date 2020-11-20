61072a61713b3a5bdf3353e01c351dc1a9c2ae62
s1n7ax <srineshnisala@gmail.com>
2020-11-20 21:46:42 +0530

    [ADD] add options
    * add vim options

--- lua/options.lua
+++ lua/options.lua
@@ -6,15 +6,28 @@ local add_options = Option.add_options;
 local global_options = {
     shiftwidth = 4,
     tabstop = 4,
+
+	-- code folding
     foldmethod = 'syntax',
+	foldlevelstart = 1,
+
+	-- enable mouse in vim
 	mouse = 'n',
-	completeopt = 'menuone,noinsert,noselect',
+
 	smartcase = true,
 	ignorecase = true,
-	wrap = false,
+	wrap = true,
 	background = 'dark',
 	termguicolors = true,
 	inccommand = 'split',
+	splitright = true,
+	splitbelow = true,
+
+	-- hide unsaved file when closing the buffer
+	-- usually vim doesn't allow closing unsaved buffer unless you force it
+	-- but with hidden option, buffer will be hidden when you close it
+	-- vim will prompt you to save when closing vim editor
+	hidden = true,
 }
 
 local window_options = {
