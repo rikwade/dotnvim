da09f6d7e10f5464a73c8e966cd155c9b9bdf638
s1n7ax <srineshnisala@gmail.com>
2020-11-20 21:45:31 +0530

    [ADD] add lsp key binds
    * added key binds to navigate within the code

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -7,6 +7,14 @@ local on_attach_callback = function(_, bufnr)
 
     local opts = { noremap = true }
     local binds = {
+        { bufnr, 'n', '<leader>a', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts },
+        { bufnr, 'n', '<leader>f', '<Cmd>lua vim.lsp.buf.formatting()<CR>', opts },
+        { bufnr, 'n', '<leader>h', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts },
+        { bufnr, 'n', '<leader>r', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts },
+
+        { bufnr, 'n', '<leader>j', '<Cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts },
+        { bufnr, 'n', '<leader>k', '<Cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts },
+
         { bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts },
         { bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts },
         { bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts },
