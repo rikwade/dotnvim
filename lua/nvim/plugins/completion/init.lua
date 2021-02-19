d6120d069aa35a7372c342b8fa10d825534dc89d
s1n7ax <srineshnisala@gmail.com>
2021-02-19 11:32:27 +0530

    [UPD] update completion config

--- lua/nvim/plugins/completion/init.lua
+++ lua/nvim/plugins/completion/init.lua
@@ -1,6 +1,23 @@
-Keybind.g({
-	-- { 'i', '<c-space>', [[<plug>(completion_trigger)]] },
-	{ 'i', '<c-j>', [[pumvisible() ? "\<C-n>" : "\<c-j>"]], { noremap = true, expr = true } },
-	{ 'i', '<c-k>', [[pumvisible() ? "\<C-p>" : "\<c-k>"]], { noremap = true, expr = true } },
+Variable.g({
+	completion_matching_strategy_list = { 'exact', 'substring', 'fuzzy', 'all' },
+	completion_matching_ignore_case = true,
+	completion_matching_smart_case = true,
+	completion_trigger_character = { '.' },
+	completion_trigger_keyword_length = 1,
+	completion_trigger_on_delete = true,
+	completion_trigger_keyword_length = 1,
+	completion_trigger_on_delete = true,
+	completion_timer_cycle = 30
 })
 
+function on_attach(bufnr)
+    Keybind.b({
+		{ bufnr, 'i', '<c-space>', [[<plug>(completion_trigger)]] },
+		{ bufnr, 'i', '<c-j>', [[pumvisible() ? "\<C-n>" : "\<c-j>"]], { noremap = true, expr = true } },
+		{ bufnr, 'i', '<c-k>', [[pumvisible() ? "\<C-p>" : "\<c-k>"]], { noremap = true, expr = true } },
+	})
+end
+
+return {
+	on_attach = on_attach
+}
