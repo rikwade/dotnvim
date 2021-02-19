9018a32b76b2dc055fec447d726c2977ea0b07c1
s1n7ax <srineshnisala@gmail.com>
2021-02-19 11:43:57 +0530

    [ADD] enable treesitter highlighting, selection & fold

---
+++ lua/nvim/plugins/treesitter/init.lua
@@ -0,0 +1,29 @@
+vim.api.nvim_exec([[
+
+	set foldmethod=expr
+	set foldexpr=nvim_treesitter#foldexpr()
+
+]], false)
+
+require('nvim-treesitter.configs').setup({
+	ensure_installed = "maintained",
+
+	highlight = {
+		enable = true,
+	},
+
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "gnn",
+			scope_incremental = "gnc",
+			node_decremental = "gnd",
+		},
+	},
+
+	indent = {
+		enable = true
+	},
+	
+})
