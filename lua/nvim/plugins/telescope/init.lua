26b0f17b272720ab999a24f371b903842713e240
s1n7ax <srineshnisala@gmail.com>
2021-02-19 18:33:00 +0530

    [ADD] [j+k] maps to close telescope window

--- lua/nvim/plugins/telescope/init.lua
+++ lua/nvim/plugins/telescope/init.lua
@@ -1,11 +1,18 @@
+local actions = require('telescope.actions')
+
 Keybind.g({
 	{ 'n', '<c-p>', '<cmd>lua require("telescope.builtin").find_files()<cr>' },
-	{ 'n', '<leader>g', '<cmd>lua require("telescope.builtin").live_grep()<cr>' },
-	{ 'n', '<leader>b', '<cmd>lua require("telescope.builtin").buffers()<cr>' },
-	{ 'n', '<leader>h', '<cmd>lua require("telescope.builtin").help_tags()<cr>' }
+	{ 'n', '<leader>tg', '<cmd>lua require("telescope.builtin").live_grep()<cr>' },
+	{ 'n', '<leader>tb', '<cmd>lua require("telescope.builtin").buffers()<cr>' },
+	{ 'n', '<leader>th', '<cmd>lua require("telescope.builtin").help_tags()<cr>' }
 })
 require('telescope').setup{
-  defaults = {
-    winblend = 15,
-  }
+    defaults = {
+        winblend = 15,
+		mappings = {
+            i = {
+                ["jk"] = actions.close,
+            },
+        },
+    }
 }
