6b353ebaf592fb72618accee573fb014af1cffb8
s1n7ax <srineshnisala@gmail.com>
2021-02-23 09:15:39 +0530

    [UPD] update vimspector keymaps

--- lua/nvim/plugins/vimspector/init.lua
+++ lua/nvim/plugins/vimspector/init.lua
@@ -2,32 +2,32 @@ local opts = { silent = true }
 
 Keybind.g({
 	-- start debugging
-	{ 'n', ';dd', ':call vimspector#Launch()<cr>', opts },
+	{ 'n', '<leader>dd', ':call vimspector#Launch()<cr>', opts },
 
 	-- reset the debug environment
 	-- removes all the windows related to vimspector
-	{ 'n', ';dD', ':call vimspector#Reset()<cr>', opts },
+	{ 'n', '<leader>dD', ':call vimspector#Reset()<cr>', opts },
 
 	-- stop debugging
-	{ 'n', ';ds', ':call vimspector#Stop()<cr>', opts },
+	{ 'n', '<leader>ds', ':call vimspector#Stop()<cr>', opts },
 
 	-- restart the debugger
-	{ 'n', ';dr', ':call vimspector#Restart()<cr>', opts },
+	{ 'n', '<leader>dr', ':call vimspector#Restart()<cr>', opts },
 
 	-- toggle breakpoint
-	{ 'n', ';t', ':call vimspector#ToggleBreakpoint()<cr>', opts },
+	{ 'n', '<leader>dt', ':call vimspector#ToggleBreakpoint()<cr>', opts },
 
 	-- step into
-	{ 'n', ';l', ':call vimspector#StepInto()<cr>', opts },
+	{ 'n', '<leader>dl', ':call vimspector#StepInto()<cr>', opts },
 
 	-- step out
-	{ 'n', ';h', ':call vimspector#StepOut()<cr>', opts },
+	{ 'n', '<leader>dh', ':call vimspector#StepOut()<cr>', opts },
 
 	-- step over
-	{ 'n', ';j', ':call vimspector#StepOver()<cr>', opts },
+	{ 'n', '<leader>j', ':call vimspector#StepOver()<cr>', opts },
 
 	-- continue
 	-- this will continue the execution unless there is a breakpoint
-	{ 'n', ';c', ':call vimspector#Continue()<cr>', opts },
+	{ 'n', '<leader>dc', ':call vimspector#Continue()<cr>', opts },
 
 })
