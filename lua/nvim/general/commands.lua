63f03724ae4ab00b211099cd64ae407b8098f395
s1n7ax <srineshnisala@gmail.com>
2021-05-01 04:15:51 +0530

    Update color theme

--- lua/nvim/general/commands.lua
+++ lua/nvim/general/commands.lua
@@ -1,4 +1,9 @@
 Command.cmd({
 	-- setting the color scheme
-	'colorscheme nightfly'
+	'colorscheme tokyonight'
 })
+
+vim.api.nvim_exec([[
+let g:tokyonight_style = 'storm' " available: night, storm
+let g:tokyonight_enable_italic = 1
+]], false)
