09265a39e2f738203baf0583c6532ec38d2f43c2
s1n7ax <srineshnisala@gmail.com>
2021-05-01 04:14:22 +0530

    Fix formatting

--- lua/nvim/plugins/init.lua
+++ lua/nvim/plugins/init.lua
@@ -3,13 +3,9 @@ require('nvim.plugins.packer.install')
 require('nvim.plugins.comment-frame')
 require('nvim.plugins.fugitive')
 require('nvim.plugins.telescope')
--- require('nvim.plugins.fzf')
--- require('nvim.plugins.lsp')
 require('nvim.plugins.nvim-tree')
--- require('nvim.plugins.nerdtree')
 require('nvim.plugins.vimspector')
-
--- require('nvim.plugins.coc-nvim')
+require('nvim.plugins.coc-nvim')
 require('nvim.plugins.completion')
 require('nvim.plugins.nvim-terminal')
 require('nvim.plugins.vim-easymotion')
@@ -19,3 +15,13 @@ require('nvim.plugins.markdown-preview')
 require('nvim.plugins.lualine')
 
 
+-- Removed in favor of telescope
+-- require('nvim.plugins.fzf')
+
+
+-- Removed in facor of nvim-tree
+-- require('nvim.plugins.nerdtree')
+
+-- Packer lazy loads the plugin configuration
+-- check install.lua for more information
+-- require('nvim.plugins.lsp')
