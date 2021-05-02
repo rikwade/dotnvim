1ac106485e40e64dc53b9b28c700081ec4b0d38b
s1n7ax <srineshnisala@gmail.com>
2021-05-03 01:15:32 +0530

    Add current buffer finder using telescope
    keybind: <leader>tf

--- lua/nvim/plugins/telescope/init.lua
+++ lua/nvim/plugins/telescope/init.lua
@@ -1,9 +1,11 @@
 local actions = require('telescope.actions')
 
+
 Keybind.g({
 	{ 'n', '<c-p>', '<cmd>lua require("telescope.builtin").find_files()<cr>' },
+	{ 'n', '<leader>tf', '<cmd>lua require("telescope.builtin").current_buffer_fuzzy_find()<cr>' },
 	{ 'n', '<leader>tg', '<cmd>lua require("telescope.builtin").live_grep()<cr>' },
-	{ 'n', '<leader>tb', '<cmd>lua require("telescope.builtin").buffers()<cr>' },
+	{ 'n', '<leader>b', '<cmd>lua require("telescope.builtin").buffers()<cr>' },
 	{ 'n', '<leader>th', '<cmd>lua require("telescope.builtin").help_tags()<cr>' }
 })
 require('telescope').setup{
@@ -14,5 +16,9 @@ require('telescope').setup{
                 ["jk"] = actions.close,
             },
         },
-    }
+		initial_mode = 'insert',
+		shorten_path = true,
+		color_devicons = true,
+		set_env = { ['COLORTERM'] = 'truecolor' },
+    },
 }
