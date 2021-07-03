817b8885c5d9ae2bf1c4e9fadc8d1fddbc40844f
s1n7ax <srineshnisala@gmail.com>
2021-07-03 23:38:11 +0530

    Add buitin LSP features
    * Added nvim-lspinstall to install LS
    * Add auto run to install servers for most existing languages
    * Remove coc in favor of builtin LSP

--- lua/nvim/plugins/completion/init.lua
+++ lua/nvim/plugins/completion/init.lua
@@ -1,3 +1,5 @@
+local completion = require('completion')
+
 Variable.g({
 	completion_matching_strategy_list = { 'exact', 'substring', 'fuzzy', 'all' },
 	completion_matching_ignore_case = true,
@@ -5,12 +7,14 @@ Variable.g({
 	completion_trigger_character = { '.' },
 	completion_trigger_keyword_length = 1,
 	completion_trigger_on_delete = true,
-	completion_trigger_keyword_length = 1,
-	completion_trigger_on_delete = true,
 	completion_timer_cycle = 30
 })
 
-function on_attach(bufnr)
+local function on_attach(bufnr)
+	-- attaches the completion callback
+    completion.on_attach()
+
+	-- add the keybind to the buffer
     Keybind.b({
 		{ bufnr, 'i', '<c-space>', [[<plug>(completion_trigger)]] },
 	})
