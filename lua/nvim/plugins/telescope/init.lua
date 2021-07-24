4ac28501a2c05e21557f7eed419b28c59ada0168
s1n7ax <srineshnisala@gmail.com>
2021-07-24 20:57:58 +0530

    Update/Add telescope keybinds

--- lua/nvim/plugins/telescope/init.lua
+++ lua/nvim/plugins/telescope/init.lua
@@ -5,8 +5,11 @@ Keybind.g({
 	{ 'n', '<c-p>', '<cmd>lua require("telescope.builtin").find_files()<cr>' },
 	{ 'n', '<leader>tf', '<cmd>lua require("telescope.builtin").current_buffer_fuzzy_find()<cr>' },
 	{ 'n', '<leader>tg', '<cmd>lua require("telescope.builtin").live_grep()<cr>' },
-	{ 'n', '<leader>b', '<cmd>lua require("telescope.builtin").buffers()<cr>' },
-	{ 'n', '<leader>th', '<cmd>lua require("telescope.builtin").help_tags()<cr>' }
+	{ 'n', '<leader>*', '<cmd>lua require("telescope.builtin").grep_string()<cr>' },
+	{ 'n', '<c-t>', '<cmd>lua require("telescope.builtin").buffers()<cr>' },
+	{ 'n', '<leader>th', '<cmd>lua require("telescope.builtin").help_tags()<cr>' },
+
+	{ 'i', '<c-r>', '<cmd>lua require("telescope.builtin").registers()<cr>' },
 })
 
 require('telescope').setup()
