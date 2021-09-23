0352230fc70692c15e4e706317649160788f6a89
s1n7ax <srineshnisala@gmail.com>
2021-09-23 20:32:48 +0530

    feat: update cmp configuration
    [CHANGES]
    * change UI
    * change source priority configuration

--- lua/nvim/plugins/nvim-cmp/init.lua
+++ lua/nvim/plugins/nvim-cmp/init.lua
@@ -1,5 +1,7 @@
-local Shortcut = R'nvim.newutil.keymap'.Shortcut
 local cmp = R 'cmp'
+local types = R 'cmp.types'
+
+local WIDE_HEIGHT = 50
 
 cmp.setup(
     {
@@ -9,7 +11,7 @@ cmp.setup(
             ['<C-d>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
             ['<C-Space>'] = cmp.mapping.complete(),
-            ['<C-e>'] = cmp.mapping.close(),
+            -- ['<C-e>'] = cmp.mapping.close(),
             ['<CR>'] = cmp.mapping.confirm(
                 {
                     behavior = cmp.ConfirmBehavior.Insert,
@@ -19,18 +21,18 @@ cmp.setup(
 
         sources = {
             {
-                name = 'snippy',
-                priority = 90,
-                max_item_count = 3,
-                keyword_length = 2,
-            },
-            {
                 name = 'nvim_lsp',
                 priority = 100,
             },
             {
                 name = 'nvim_lua',
                 priority = 100,
+            },
+            {
+                name = 'snippy',
+                priority = 90,
+                max_item_count = 3,
+                keyword_length = 2,
             },
             {
                 name = 'buffer',
@@ -59,10 +61,19 @@ cmp.setup(
                 R'snippy'.expand_snippet(args.body)
             end,
         },
-        completion = {
-            keyword_length = 5,
-        },
-        preselect = true,
-    })
 
-Shortcut:keymaps():map({ { '<c-e>', cmp.mapping.close } })
+        completion = {
+            keyword_length = 2,
+            completeopt = 'menu,menuone,preview,noinsert',
+        },
+
+        preselect = true,
+
+        documentation = {
+            border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+            winhighlight = 'NormalFloat:NormalFloat,FloatBorder:NormalFloat',
+            maxwidth = math.floor(
+                (WIDE_HEIGHT * 2) * (V.o.columns / (WIDE_HEIGHT * 2 * 16 / 9))),
+            maxheight = math.floor(WIDE_HEIGHT * (WIDE_HEIGHT / V.o.lines)),
+        },
+    })
