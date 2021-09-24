8fa5744576ce67c28f7b6eafc22b5e4c3f1893f8
s1n7ax <srineshnisala@gmail.com>
2021-09-24 10:42:09 +0530

    refactor: update telescope keymaps

--- lua/nvim/plugins/telescope/keymaps.lua
+++ lua/nvim/plugins/telescope/keymaps.lua
@@ -1,4 +1,4 @@
-local Shortcut = R'nvim.newutil.keymap'
+local Shortcut = R 'nvim.newutil.keymap'
 local l = Keybind.get_lua_cmd_string
 
 local builtin = function(function_name)
@@ -28,10 +28,10 @@ Shortcut:mode('n'):options():noremap():next():keymaps(
         { '<leader>f', builtin 'current_buffer_fuzzy_find()' },
 
         -- find word under the cursor in current buffer
-        { '*', builtin 'grep_string()' },
+        { '<leader>*', builtin 'grep_string()' },
 
         -- find help index
-        { '.th', builtin 'help_tags()' },
+        { '<leader>h', builtin 'help_tags()' },
 
         ----------------------------------------------------------------------
         --                               LSP                                --
@@ -39,8 +39,15 @@ Shortcut:mode('n'):options():noremap():next():keymaps(
         -- find references of word under the cursor
         { '<leader>o', builtin 'lsp_references()' },
 
-        -- find diagnostics in the project
-        { '.td', builtin 'lsp_workspace_diagnostics()' },
+        -- find diagnostics in the file
+        { '<leader>d', builtin 'lsp_document_diagnostics()' },
+
+        -- show code actions
+        { '<leader>a', builtin 'lsp_range_code_actions()' },
+
+        { '<leader>c', builtin 'lsp_definitions()' },
+
+        { '<leader>m', builtin 'lsp_implementations()' },
     })
 
 Shortcut:mode('i'):options():noremap():next():keymaps(
@@ -48,3 +55,18 @@ Shortcut:mode('i'):options():noremap():next():keymaps(
         -- find register by containing value
         { '<c-r>', builtin 'registers()' },
     })
+
+--[[
+  loclist = <function 25>,
+  lsp_code_actions = <function 26>,
+  lsp_definitions = <function 27>,
+  lsp_document_diagnostics = <function 28>,
+  lsp_document_symbols = <function 29>,
+  lsp_dynamic_workspace_symbols = <function 30>,
+  lsp_implementations = <function 31>,
+  lsp_range_code_actions = <function 32>,
+  lsp_references = <function 33>,
+  lsp_type_definitions = <function 34>,
+  lsp_workspace_diagnostics = <function 35>,
+  lsp_workspace_symbols = <function 36>,
+  --]]
