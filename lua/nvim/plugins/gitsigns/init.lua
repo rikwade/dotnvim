dbd1a09d68d0e4f00ae979d67c6f7b1d2edb9786
s1n7ax <srineshnisala@gmail.com>
2021-09-26 19:28:22 +0530

    fix: fix gitsign prop change in new update

--- lua/nvim/plugins/gitsigns/init.lua
+++ lua/nvim/plugins/gitsigns/init.lua
@@ -37,7 +37,7 @@ R'gitsigns'.setup(
             ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
             ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
         },
-        watch_index = { interval = 1000, follow_files = true },
+        watch_gitdir = { interval = 1000, follow_files = true },
         sign_priority = 6,
         update_debounce = 100,
         status_formatter = nil, -- Use default
