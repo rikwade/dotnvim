648d61655158b9b1f461b5035630a0cbac0ab858
s1n7ax <srineshnisala@gmail.com>
2021-09-28 22:33:41 +0530

    fix: avoid joining paths when taking file under cursor

--- lua/nvim/utils/nvim/file.lua
+++ lua/nvim/utils/nvim/file.lua
@@ -6,35 +6,25 @@ local M = {}
 
 --[[
 -- Reteruns the file path and line number (if exists) under the cursor
--- IF the file does not exists, function will return null
 --
 -- @return { (file: string, line: number) }
 --]]
-local function get_file_and_line_under_the_cursor()
+function M.get_file_and_line_under_the_cursor()
     local isfname = vim.o.isfname
     vim.opt.isfname:append(':')
 
     -- get the file path including line number
-    local file_obj = vim.fn.expand('<cfile>:p')
-    if file_obj == '' then return end
+    local text_object = vim.fn.expand('<cfile>')
+    if text_object == '' then return end
 
-    local head = vim.fn.fnamemodify(file_obj, ':h')
-    local tail = vim.fn.fnamemodify(file_obj, ':t')
-    local name = tail
+    local file = text_object
     local line = nil
 
     -- extract the line number if exists
-    if str_util.contains(tail, ':') then
-        local split = str_util.split(tail, ':')
-        Log.ins(split)
-        name = split[1]
-        line = split[2]
+    if text_object:match(':%d+$') then
+        file = text_object:gsub(':%d+$', '')
+        line = tonumber(text_object:match('%d+$'))
     end
-
-    local file = head .. '/' .. name
-
-    -- stop if the file does not exist
-    if vim.fn.filereadable(file) == 0 then return end
 
     vim.o.isfname = isfname
 
@@ -47,19 +37,25 @@ end
 -- IF the file not found, function will stop
 --]]
 function M.open_file_under_cursor()
-    local file_obj = get_file_and_line_under_the_cursor()
+    local text_object = get_file_and_line_under_the_cursor()
 
-    if not file_obj then return end
+    if not text_object then return end
 
-    local file = file_obj[1]
-    local line = file_obj[2]
+    local file = text_object[1]
+    local line = text_object[2]
+
+    -- stop if the file does not exist
+    if vim.fn.filereadable(file) == 0 then return end
 
     local selectable = wpicker.filter_windows()
 
     if #selectable < 2 then
         vim.cmd('vsp ' .. file)
     else
-        local window = wpicker.pick_window({ include_current_win = true })
+        local window = wpicker.pick_window(
+                           {
+                include_current_win = true,
+            })
 
         if window then
             vim.api.nvim_set_current_win(window)
