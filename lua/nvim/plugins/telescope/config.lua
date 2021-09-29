e2bdd2f57d6df75a44beab35feeae0c8a42c6c87
s1n7ax <srineshnisala@gmail.com>
2021-09-29 16:03:44 +0530

    feat: telescope show file name always and path if possible

--- lua/nvim/plugins/telescope/config.lua
+++ lua/nvim/plugins/telescope/config.lua
@@ -1,2 +1,8 @@
-R'telescope'.setup()
+R'telescope'.setup(
+    {
+        defaults = {
+            path_display = { 'truncate', 'tail' },
+            file_ignore_patterns = {},
+        },
+    })
 R'telescope'.load_extension('fzf')
