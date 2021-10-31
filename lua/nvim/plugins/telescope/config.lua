650423f3ac3b19732df6fa1ee85dc25e04d91281
s1n7ax <srineshnisala@gmail.com>
2021-10-31 10:25:13 +0530

    feat: ignore nvim pack/plugin dir in telescope

--- lua/nvim/plugins/telescope/config.lua
+++ lua/nvim/plugins/telescope/config.lua
@@ -2,7 +2,12 @@ R'telescope'.setup(
     {
         defaults = {
             path_display = { 'truncate' },
-            file_ignore_patterns = { 'dist', 'target' },
+            file_ignore_patterns = {
+                'dist',
+                'target',
+                'node_modules',
+                'pack/plugins',
+            },
         },
     })
 R'telescope'.load_extension('fzf')
