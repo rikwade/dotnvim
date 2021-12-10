add2ecd30520b279d00ed4ff096347e496cf9df6
s1n7ax <srineshnisala@gmail.com>
2021-12-10 11:14:10 +0530

    refactor: next and prev key changes in cmp and telescope

--- lua/nvim/plugins/telescope/config.lua
+++ lua/nvim/plugins/telescope/config.lua
@@ -7,6 +7,14 @@ require('telescope').setup({
             'node_modules',
             'pack/plugins',
         },
+
+        mappings = {
+            i = {
+                ['<C-h>'] = 'which_key',
+                ['<C-n>'] = 'move_selection_next',
+                ['<C-e>'] = 'move_selection_previous',
+            },
+        },
     },
 })
 require('telescope').load_extension('fzf')
