66bcc9eeec573e02de52c2f55eae1ad214564a1b
s1n7ax <srineshnisala@gmail.com>
2022-02-10 20:36:13 +0530

    feat: add luasnip virtual text icons

--- lua/nvim/plugins/luasnip/init.lua
+++ lua/nvim/plugins/luasnip/init.lua
@@ -1,5 +1,30 @@
 local Shortcut = require('nvim.utils.nvim.shortcut')
+local Highlighter = require('nvim.utils.nvim.highlighting.highlighter')
+local HighlightGroups = require('nvim.utils.nvim.highlighting.highlight-groups')
 local ls = require('luasnip')
+local types = require('luasnip.util.types')
+
+local highlighter = Highlighter:new():add(HighlightGroups({
+    LuaSnipChoiseNode = 'guifg=#f0a535',
+    LuaSnipActiveNode = 'guifg=#49e635',
+}))
+
+highlighter:register_highlights()
+
+ls.config.setup({
+    ext_opts = {
+        [types.choiceNode] = {
+            active = {
+                virt_text = { { '●', 'LuaSnipChoiseNode' } },
+            },
+        },
+        [types.insertNode] = {
+            active = {
+                virt_text = { { '●', 'LuaSnipActiveNode' } },
+            },
+        },
+    },
+})
 
 Shortcut
     :mode({ 'i', 's' })
