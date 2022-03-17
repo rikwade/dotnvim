258a77678a22d4c3b8637d8300d12d09f0986e18
s1n7ax <srineshnisala@gmail.com>
2022-03-18 00:06:20 +0530

    feat: add string utils

--- lua/nvim/utils/lua/string.lua
+++ lua/nvim/utils/lua/string.lua
@@ -34,4 +34,28 @@ function M.is_string(value)
     return type(value) == 'string'
 end
 
+function M.get_space_str(number_of_spaces)
+    return string.format('%%-%ds', number_of_spaces):format('')
+end
+
+function M.get_line_iter(str)
+    if str:sub(-1) ~= '\n' then
+        str = str .. '\n'
+    end
+
+    return str:gmatch('(.-)\n')
+end
+
+function M.box_trim_lines(str)
+    local new_str = ''
+
+    for line in M.get_line_iter(str) do
+        line = line:gsub('^%s+', '')
+        line = string.gsub(line, '%s+$', '')
+        new_str = new_str .. '\n' .. line
+    end
+
+    return new_str
+end
+
 return M
