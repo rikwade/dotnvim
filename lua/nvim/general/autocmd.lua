5963d1e05ddaad997da4885be7cbec0357d5eb7e
s1n7ax <srineshnisala@gmail.com>
2022-04-03 21:20:49 +0530

    refactor: highlight on yank aucmd to lua syntax

--- lua/nvim/general/autocmd.lua
+++ lua/nvim/general/autocmd.lua
@@ -1,10 +1,11 @@
-local api = vim.api
-
--- highlight the current cursor line
-api.nvim_exec(
-    [[
-    " highlighting on yank
-    au TextYankPost * lua vim.highlight.on_yank {higroup="IncSearch", timeout=300, on_visual=true}
-]],
-    false
-)
+-- highlight text on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 300,
+            on_visual = true,
+        })
+    end,
+})
