6020c497c3846640f02a52f3787910b7b31edfc0
s1n7ax <srineshnisala@gmail.com>
2022-04-09 15:07:39 +0530

    feat: add module snippet

--- lua/nvim/plugins/luasnip/snippets/lua/snip_module.lua
+++ lua/nvim/plugins/luasnip/snippets/lua/snip_module.lua
@@ -6,26 +6,56 @@ local s = ls.s
 local rep = require('luasnip.extras').rep
 local fmt = require('luasnip.extras.fmt').fmt
 local i = ls.insert_node
+local c = ls.choice_node
+local t = ls.text_node
 
 local tl = su.box_trim_lines
 
 local sm = SnippetManager('lua')
 
+local indentation = su.get_space_str(vim.opt.softtabstop:get())
+
 -- creates a new module
 sm:add_snippet(s(
     'mod',
-    fmt(
-        tl([[
-            local {} = {{}}
-            
-            {}
+    fmt('{}', {
+        c(1, {
+            fmt(
+                tl([[
+                local {} = {{}}
+                
+                {}
 
-            return {}
-        ]]),
-        {
-            i(1, 'M'),
-            i(2),
-            rep(1),
-        }
-    )
+                return {}
+            ]]),
+                {
+                    i(1, 'M'),
+                    i(2),
+                    rep(1),
+                }
+            ),
+
+            fmt(
+                tl([[
+                local {} = {{}}
+                {}.__index = {}
+
+                function {}.new()
+                    {}return setmetatable({{}}, {})
+                end
+
+                return {}
+            ]]),
+                {
+                    i(1, 'M'),
+                    rep(1),
+                    rep(1),
+                    rep(1),
+                    t(indentation),
+                    rep(1),
+                    rep(1),
+                }
+            ),
+        }),
+    })
 ))
