d47ac8cfc090d835ecf18149d5c89f86f4638820
s1n7ax <srineshnisala@gmail.com>
2022-05-12 22:20:32 +0530

    feat: incline style change for better visibility

--- lua/nvim/plugins/incline/init.lua
+++ lua/nvim/plugins/incline/init.lua
@@ -1,9 +1,32 @@
+local Highlighter = require('nvim.utils.nvim.highlighting.highlighter')
+local HighlightGroups = require('nvim.utils.nvim.highlighting.highlight-groups')
+local ThemeManager = require('nvim.utils.nvim.theme.theme-manager')
+
+local theme = ThemeManager.get_theme()
+
+local incline_highlights = HighlightGroups({
+    InclineNormal = { guifg = theme.bright.black, guibg = theme.bright.white },
+})
+
+Highlighter:add(incline_highlights):register_highlights()
+
 require('incline').setup({
     window = {
         zindex = 1000,
         placement = {
-            horizontal = 'center',
+            horizontal = 'right',
             vertical = 'top',
+        },
+        padding = {
+            left = 5,
+            right = 5,
+        },
+    },
+
+    highlight = {
+        groups = {
+            InclineNormal = 'InclineNormal',
+            InclineNormalNC = 'InclineNormal',
         },
     },
 })
