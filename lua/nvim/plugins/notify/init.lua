03c8493d56948bfdefbc839a2add976b2d215258
s1n7ax <srineshnisala@gmail.com>
2022-05-14 23:35:32 +0530

    refactor: keep notify win above all (zindex)

--- lua/nvim/plugins/notify/init.lua
+++ lua/nvim/plugins/notify/init.lua
@@ -2,6 +2,9 @@ local notify = require('notify')
 
 notify.setup({
     timeout = 2000,
+    on_open = function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 2000 })
+    end,
 })
 
 vim.notify = notify
