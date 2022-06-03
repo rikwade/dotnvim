a50c2e504f3c533878e08c880e3829badae17268
s1n7ax <srineshnisala@gmail.com>
2022-06-03 08:45:31 +0530

    feat(ui): use theme color for on yank highlight

--- lua/nvim/general/autocmd.lua
+++ lua/nvim/general/autocmd.lua
@@ -1,9 +1,21 @@
+local ThemeManager = require('nvim.utils.nvim.theme.theme-manager')
+local HighlightGroups = require('nvim.utils.nvim.highlighting.highlight-groups')
+local highlighter = require('nvim.utils.nvim.highlighting.highlighter')
+
+local theme = ThemeManager.get_theme()
+
+local highlight_groups = HighlightGroups({
+    TextYank = { guibg = theme.normal.yellow, guifg = theme.normal.black },
+})
+
+highlighter:new():add(highlight_groups):register_highlights()
+
 -- highlight text on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
     pattern = '*',
     callback = function()
         vim.highlight.on_yank({
-            higroup = 'IncSearch',
+            higroup = highlight_groups.TextYank.name,
             timeout = 300,
             on_visual = true,
         })
