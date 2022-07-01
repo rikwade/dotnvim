1f43b9aaa55bbea2b5afb63b49033c1b39869a36
s1n7ax <srineshnisala@gmail.com>
2022-07-01 23:41:38 +0530

    refactor: add snippet support back to cmp
    Without snippet support, cmp does not complete brackets of functions

--- lua/nvim/plugins/lsp/common/cmp-capability.lua
+++ lua/nvim/plugins/lsp/common/cmp-capability.lua
@@ -9,8 +9,6 @@ function M.setup()
             vim.lsp.protocol.make_client_capabilities()
         )
 
-        capabilities.textDocument.completion.completionItem.snippetSupport = false
-
         conf:set_option('capabilities', capabilities)
     end)
 end
