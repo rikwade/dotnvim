161e1af81470db2ce8009168d4760619b6711668
s1n7ax <srineshnisala@gmail.com>
2022-07-10 02:07:32 +0530

    feat: dap re-configure before dap.continue()

--- lua/nvim/plugins/nvim-dap/keymap.lua
+++ lua/nvim/plugins/nvim-dap/keymap.lua
@@ -1,18 +1,60 @@
+local dap = require('dap')
 local Shortcut = require('nvim.utils.nvim.shortcut')
 local Lsp = require('nvim.utils.lsp')
 local Event = require('nvim.utils.lsp.event')
 local ConfEvent = require('nvim.utils.lsp.config.event')
 local widgets = require('dap.ui.widgets')
+local JavaDap = require('nvim.utils.dap.java')
+local Notify = require('nvim.utils.notify')
+
+local java_dap = JavaDap()
+
+local notify = Notify({
+    title = 'Debugger',
+})
 
 local M = {}
 
-function M.on_attach(_, buffer)
+function M.on_attach(ls, buffer)
+    local nmap = Shortcut():mode('n'):options():buffer(buffer):noremap():next()
+
+    ----------------------------------------------------------------------
+    --                    Language specific keymaps                     --
+    ----------------------------------------------------------------------
+    if ls.name == 'jdtls' then
+        nmap:keymaps({
+            {
+                ',tc',
+                function()
+                    java_dap
+                        :get_dap_config()
+                        :thenCall(function(conf)
+                            dap.configurations.java = conf
+                            dap.continue()
+                        end)
+                        :catch(function(reason)
+                            local message = 'Something went wrong'
+
+                            if type(reason) == 'string' then
+                                message = reason
+                            elseif type(reason) == 'table' then
+                                if reason.message then
+                                    message = reason.message
+                                end
+                            end
+
+                            notify:error('Java Dap setup failed! ' .. message)
+                        end)
+                end,
+            },
+        })
+    end
+
     Shortcut():mode('n'):options():buffer(buffer):noremap():next():keymaps({
         -- close the debug session
         { ',tS', require('dap').terminate },
 
         -- start the debug session and continue to next breakpoint
-        { ',tc', require('dap').continue },
 
         -- run last again
         { ',tl', require('dap').run_last },
