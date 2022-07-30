9d7d73a55c4ef1cf3b35dcbe7ca2bf57281d1567
s1n7ax <srineshnisala@gmail.com>
2022-07-30 12:46:48 +0530

    refactor: remove space & eol chars from indent lines

--- lua/nvim/plugins/indent-blankline/init.lua
+++ lua/nvim/plugins/indent-blankline/init.lua
@@ -15,10 +15,6 @@ local group = HighlightGroup({
 
 Highlighter:new():add(group):register_highlights()
 
-vim.opt.list = true
-vim.opt.listchars:append('space:⋅')
-vim.opt.listchars:append('eol:↴')
-
 require('indent_blankline').setup({
     space_char_blankline = ' ',
     char_highlight_list = {
