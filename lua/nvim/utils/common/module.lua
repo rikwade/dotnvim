84c911a4610232f0aa26c018f2936ecf85e87894
s1n7ax <srineshnisala@gmail.com>
2022-08-09 10:21:45 +0530

    refactor: unload_module uses starts with text instead contains

--- lua/nvim/utils/common/module.lua
+++ lua/nvim/utils/common/module.lua
@@ -1,12 +1,8 @@
-local Pattern = require('nvim.utils.common.pattern')
-
 local M = {}
 
 function M.unload_package(package_name)
-    local esc_package_name = Pattern.escape_pattern(package_name)
-
     for module_name, _ in pairs(package.loaded) do
-        if string.find(module_name, esc_package_name) then
+        if string.find(module_name, '^' .. package_name) then
             package.loaded[module_name] = nil
         end
     end
