b0cbdeca44c80dee83bfbece671523bb62837c62
s1n7ax <srineshnisala@gmail.com>
2022-08-21 14:27:38 +0530

    refactor: gitsigns config update

--- lua/nvim/plugins/gitsigns/init.lua
+++ lua/nvim/plugins/gitsigns/init.lua
@@ -1,40 +1,62 @@
 require('gitsigns').setup({
-    keymaps = {
-        noremap = true,
-
-        ['n ]c'] = {
-            expr = true,
-            "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'",
-        },
-        ['n [c'] = {
-            expr = true,
-            "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'",
-        },
-
-        ['n <leader>gs'] = '<cmd>Gitsigns stage_hunk<CR>',
-        ['v <leader>gs'] = ':Gitsigns stage_hunk<CR>',
-        ['n <leader>gu'] = '<cmd>Gitsigns undo_stage_hunk<CR>',
-        ['n <leader>gr'] = '<cmd>Gitsigns reset_hunk<CR>',
-        ['v <leader>gr'] = ':Gitsigns reset_hunk<CR>',
-        ['n <leader>gR'] = '<cmd>Gitsigns reset_buffer<CR>',
-        ['n <leader>gp'] = '<cmd>Gitsigns preview_hunk<CR>',
-        ['n <leader>gb'] = '<cmd>lua require"gitsigns".blame_line{full=true}<CR>',
-        ['n <leader>gS'] = '<cmd>Gitsigns stage_buffer<CR>',
-        ['n <leader>gU'] = '<cmd>Gitsigns reset_buffer_index<CR>',
-
-        -- Text objects
-        ['o kg'] = ':<C-U>Gitsigns select_hunk<CR>',
-        ['x kg'] = ':<C-U>Gitsigns select_hunk<CR>',
-    },
-    watch_gitdir = {
-        interval = 1000,
-        follow_files = true,
-    },
-    current_line_blame = true,
+    signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+    numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+    linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+    word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = 'eol',
-        delay = 1000,
-        ignore_whitespace = false,
+        delay = 3000,
     },
+    yadm = {
+        enable = false,
+    },
+
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+            if vim.wo.diff then
+                return ']c'
+            end
+            vim.schedule(function()
+                gs.next_hunk()
+            end)
+            return '<Ignore>'
+        end, { expr = true })
+
+        map('n', '[c', function()
+            if vim.wo.diff then
+                return '[c'
+            end
+            vim.schedule(function()
+                gs.prev_hunk()
+            end)
+            return '<Ignore>'
+        end, { expr = true })
+
+        -- Actions
+        map('n', '<leader>hp', gs.preview_hunk)
+        map('n', '<leader>hb', function()
+            gs.blame_line({ full = false })
+        end)
+        map('n', '<leader>hB', function()
+            gs.blame_line({ full = true })
+        end)
+        map('n', '<leader>tb', gs.toggle_current_line_blame)
+        map('n', '<leader>hd', gs.diffthis)
+        map('n', '<leader>hD', function()
+            gs.diffthis('~')
+        end)
+        map('n', '<leader>td', gs.toggle_deleted)
+
+        -- Text object
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    end,
 })
