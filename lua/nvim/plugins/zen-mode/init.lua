f821635b96f45857fd2f352a44498fa314f6d65f
s1n7ax <srineshnisala@gmail.com>
2022-08-21 15:27:49 +0530

    refactor: disable statusline on zen-mode

--- lua/nvim/plugins/zen-mode/init.lua
+++ lua/nvim/plugins/zen-mode/init.lua
@@ -1,27 +1,16 @@
 local Shortcut = require('nvim.utils.nvim.shortcut')
 
+local laststatus = vim.o.laststatus
+
 require('zen-mode').setup({
-    window = {
-        backdrop = 0.95,
-        width = 120,
-        height = 1,
-        options = {
-            signcolumn = 'yes:1',
-            number = true,
-            relativenumber = true,
-            cursorline = true,
-            cursorcolumn = false,
-            foldcolumn = '1',
-            list = false,
-        },
-    },
-    plugins = {
-        options = { enabled = true, ruler = true, showcmd = true },
-        twilight = { enabled = true },
-        gitsigns = { enabled = true },
-    },
+    on_open = function()
+        laststatus = vim.o.laststatus
+        vim.o.laststatus = 0
+    end,
+    -- callback where you can add custom code when the Zen window closes
+    on_close = function()
+        vim.laststatus = laststatus
+    end,
 })
 
-Shortcut()
-    :mode('n')
-    :keymap('.a', ':ZenMode<CR>')
+Shortcut():mode('n'):keymap('.a', ':ZenMode<CR>')
