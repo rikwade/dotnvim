38e080243aad22d65e18efd75020c74165840fe6
s1n7ax <srineshnisala@gmail.com>
2022-09-27 14:56:25 +0530

    refactor: telescope keymap to lua functions

--- lua/nvim/plugins/nvim-telescope/config.lua
+++ lua/nvim/plugins/nvim-telescope/config.lua
@@ -1,4 +1,5 @@
 local telescope = require('telescope')
+local action = require('telescope.actions')
 
 telescope.setup({
     defaults = {
@@ -12,9 +13,11 @@ telescope.setup({
 
         mappings = {
             i = {
-                ['<C-h>'] = 'which_key',
-                ['<C-n>'] = 'move_selection_next',
-                ['<C-e>'] = 'move_selection_previous',
+                ['<C-h>'] = action.which_key,
+                ['<C-n>'] = action.move_selection_next,
+                ['<C-e>'] = action.move_selection_previous,
+                ['<C-c>'] = action.close,
+                ['<esc>'] = action.close,
             },
         },
     },
