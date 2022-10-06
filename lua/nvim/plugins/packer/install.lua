d160f0983654a3e06b9d097751a7add589252de1
s1n7ax <srineshnisala@gmail.com>
2022-10-06 16:55:32 +0530

    refactor: replace vim smoothie with neoscroll
    vim smoothie does not scroll well when there are folds

--- lua/nvim/plugins/packer/install.lua
+++ lua/nvim/plugins/packer/install.lua
@@ -266,7 +266,12 @@ return require('packer').startup(function()
     use({ 'unblevable/quick-scope' })
 
     -- smooth scrolling
-    use({ 'psliwka/vim-smoothie' })
+    use({
+        'karb94/neoscroll.nvim',
+        config = function()
+            require('neoscroll').setup()
+        end,
+    })
 
     -- motion
     use({
