0593cad884564f700d5a32685803e9fcd7c0dac8
s1n7ax <srineshnisala@gmail.com>
2022-10-09 17:43:17 +0530

    fix: error when creating a node in neo-tree

--- lua/nvim/general/autocmd.lua
+++ lua/nvim/general/autocmd.lua
@@ -26,6 +26,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.api.nvim_create_autocmd('BufWinEnter', {
     pattern = '*',
     callback = function()
+        -- skip if a pop up window
+        if vim.fn.win_gettype() == 'popup' then
+            return
+        end
+
+        -- skip if new buffer
         if vim.bo.filetype == '' then
             return
         end
