18ee53b397bf2f8219fa5a905064a47cdaeb43b8
s1n7ax <srineshnisala@gmail.com>
2022-10-13 10:01:58 +0530

    fix: window switch keymaps not working
    Keymaps are overlapping with the neoscroll keymaps.
    So removed some overlapping keymaps

--- lua/nvim/plugins/packer/install.lua
+++ lua/nvim/plugins/packer/install.lua
@@ -269,7 +269,7 @@ return require('packer').startup(function()
     use({
         'karb94/neoscroll.nvim',
         config = function()
-            require('neoscroll').setup()
+            require('nvim.plugins.neoscroll')
         end,
     })
 
