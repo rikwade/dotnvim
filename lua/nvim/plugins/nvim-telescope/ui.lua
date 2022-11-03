205f8069d6a0ce39c222f4886b0df650d58c09be
s1n7ax <srineshnisala@gmail.com>
2022-11-03 08:42:02 +0530

    refactor: remove telescope method rename for hl changes

--- lua/nvim/plugins/nvim-telescope/ui.lua
+++ lua/nvim/plugins/nvim-telescope/ui.lua
@@ -1,10 +1,4 @@
-local get = function(name)
-    return vim.api.nvim_get_hl_by_name(name, true)
-end
-
-local set = vim.api.nvim_set_hl
-
-local normal_hl = get('Normal')
+local normal_hl = vim.api.nvim_get_hl_by_name('Normal', true)
 
 local purple1 = '#333352'
 local purple2 = '#232338'
@@ -16,27 +10,27 @@ local blue1 = '#0985de'
 ----------------------------------------------------------------------
 --                              Prompt                              --
 ----------------------------------------------------------------------
-set(0, 'TelescopePromptBorder', {
+vim.api.nvim_set_hl(0, 'TelescopePromptBorder', {
     fg = purple3,
     bg = purple3,
 })
 
-set(0, 'TelescopePromptNormal', {
+vim.api.nvim_set_hl(0, 'TelescopePromptNormal', {
     fg = normal_hl.foreground,
     bg = purple3,
 })
 
-set(0, 'TelescopePromptTitle', {
+vim.api.nvim_set_hl(0, 'TelescopePromptTitle', {
     fg = normal_hl.foreground,
     bg = red1,
 })
 
-set(0, 'TelescopePromptCounter', {
+vim.api.nvim_set_hl(0, 'TelescopePromptCounter', {
     fg = red1,
     bg = purple3,
 })
 
-set(0, 'TelescopePromptPrefix', {
+vim.api.nvim_set_hl(0, 'TelescopePromptPrefix', {
     fg = red1,
     bg = purple3,
 })
@@ -44,41 +38,41 @@ set(0, 'TelescopePromptPrefix', {
 ----------------------------------------------------------------------
 --                              Result                              --
 ----------------------------------------------------------------------
-set(0, 'TelescopeResultsBorder', {
+vim.api.nvim_set_hl(0, 'TelescopeResultsBorder', {
     fg = purple2,
     bg = purple2,
 })
 
-set(0, 'TelescopeResultsNormal', {
+vim.api.nvim_set_hl(0, 'TelescopeResultsNormal', {
     fg = normal_hl.foreground,
     bg = purple2,
 })
 
-set(0, 'TelescopeResultsTitle', {
+vim.api.nvim_set_hl(0, 'TelescopeResultsTitle', {
     fg = normal_hl.foreground,
     bg = blue1,
 })
 
-set(0, 'TelescopeSelectionCaret', {
+vim.api.nvim_set_hl(0, 'TelescopeSelectionCaret', {
     fg = blue1,
-    bg = get('TelescopeSelection').background,
+    bg = vim.api.nvim_get_hl_by_name('TelescopeSelection', true).background,
 })
 
 ----------------------------------------------------------------------
 --                             Preview                              --
 ----------------------------------------------------------------------
 
-set(0, 'TelescopePreviewBorder', {
+vim.api.nvim_set_hl(0, 'TelescopePreviewBorder', {
     fg = purple1,
     bg = purple1,
 })
 
-set(0, 'TelescopePreviewNormal', {
+vim.api.nvim_set_hl(0, 'TelescopePreviewNormal', {
     fg = normal_hl.foreground,
     bg = purple1,
 })
 
-set(0, 'TelescopePreviewTitle', {
+vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', {
     fg = normal_hl.foreground,
     bg = green1,
 })
