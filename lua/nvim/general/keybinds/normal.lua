021e519c58b1eba32920f85c0967d528c4bc2e0f
s1n7ax <srineshnisala@gmail.com>
2022-12-09 11:49:13 +0530

    refactor: switching to which key for normal keymaps

--- lua/nvim/general/keybinds/normal.lua
+++ lua/nvim/general/keybinds/normal.lua
@@ -1,101 +1,78 @@
-local Shortcut = require('nvim.utils.nvim.shortcut')
 local common = require('nvim.utils.common.module')
 local window = require('nvim.utils.nvim.window')
+local wk = require('which-key')
 
-Shortcut():mode('n'):options():noremap():next():keymaps({
-    ----------------------------------------------------------------------
-    --                              CONFIG                              --
-    ----------------------------------------------------------------------
-    -- reload nvim configuration file
-    {
-        '<tab>r',
+----------------------------------------------------------------------
+--                              CONFIG                              --
+----------------------------------------------------------------------
+wk.register({
+    r = {
         function()
             common.reload_package('nvim')
         end,
+        'Reload config',
     },
+}, {
+    prefix = '<tab>',
+})
 
-    ----------------------------------------------------------------------
-    --                           CURSOR MOVE                            --
-    ----------------------------------------------------------------------
-    -- move cursor to left window
-    { '<c-m>', '<c-w>h' },
+----------------------------------------------------------------------
+--                           CURSOR MOVE                            --
+----------------------------------------------------------------------
+wk.register({
+    ['<c-m>'] = { '<c-w>h', 'Jump left window' },
+    ['<c-i>'] = { '<c-w>l', 'Jump right window' },
+    ['<c-n>'] = { '<c-w>j', 'Jump bottom window' },
+    ['<c-e>'] = { '<c-w>k', 'Jump top window' },
+    ['<c-w><c-w>'] = { '<c-w>p', 'Jump to last window' },
 
-    -- move cursor to right window
-    { '<c-i>', '<c-w>l' },
+    ["''"] = { '``', 'Jump to last jump point' },
 
-    -- move cursor to bottom window
-    { '<c-n>', '<c-w>j' },
-
-    -- move cursor to top window
-    { '<c-e>', '<c-w>k' },
-
-    -- move cursor to last window
-    { '<c-w><c-w>', '<c-w>p' },
-
-    -- move to last cursor position in the jump list
-    { [['']], '``' },
-
-    -- to go to the start of the line
-    { '0', '^' },
+    ['0'] = { '^', 'Jump to line start (non-blank)' },
 
     -- centering the cursor after action
-    { '{', '{zz' },
-    { '}', '}zz' },
-    { 'k', 'nzz' },
-    { 'K', 'Nzz' },
+    ['{'] = { '{zz', 'Previous empty line' },
+    ['}'] = { '}zz', 'Next empty line' },
+    ['<c-l>'] = { '<c-i>', 'Jump prev jump point' },
+}, {})
 
+----------------------------------------------------------------------
+--                        BUFFER READ WRITE                         --
+----------------------------------------------------------------------
+wk.register({
+    [',w'] = { '<cmd>silent w<cr>', 'Save file' },
+    [',q'] = { '<cmd>q<cr>', 'Close window' },
+    [',Q'] = { '<cmd>tabclose<cr>', 'Close tab' },
+})
 
-    ----------------------------------------------------------------------
-    --                        BUFFER READ WRITE                         --
-    ----------------------------------------------------------------------
-    -- save the current buffer
-    { ',w', '<cmd>silent w<cr>' },
-
-    -- close the window
-    { ',q', '<cmd>q<cr>' },
-
-    -- close the tab
-    { ',Q', '<cmd>tabclose<cr>' },
-
-    ----------------------------------------------------------------------
-    --                              JUMPS                               --
-    ----------------------------------------------------------------------
-    { '<c-l>', '<c-i>' },
-
-    ----------------------------------------------------------------------
-    --                      BUFFER & WINDOW SWITCH                      --
-    ----------------------------------------------------------------------
-    -- open the file under the cursor in a split or selected window
-    {
-        'gf',
+----------------------------------------------------------------------
+--                      BUFFER & WINDOW SWITCH                      --
+----------------------------------------------------------------------
+wk.register({
+    gf = {
         function()
             require('nvim.utils.nvim.file').open_file_under_cursor()
         end,
+        'Open file on cursor',
     },
 
-    -- open the file under the cursor in the same window
-    { 'gF', 'gf' },
+    gF = { 'gf', 'Open file on cursor (same window)' },
+})
 
-    ----------------------------------------------------------------------
-    --                          CUT COPY PASTE                          --
-    ----------------------------------------------------------------------
-    -- copy current line and paste next line
-    { '<leader>y', 'yyp' },
+----------------------------------------------------------------------
+--                           SPLIT WINDOW                           --
+----------------------------------------------------------------------
+wk.register({
+    ['<tab>h'] = { window.split_left, 'Split left' },
+    ['<tab>n'] = { window.split_bottom, 'Split bottom' },
+    ['<tab>e'] = { window.split_top, 'Split top' },
+    ['<tab>i'] = { window.split_right, 'Split right' },
+})
 
-    ----------------------------------------------------------------------
-    --                           SPLIT WINDOW                           --
-    ----------------------------------------------------------------------
-    { '<tab>h', window.split_left },
-
-    { '<tab>n', window.split_bottom },
-
-    { '<tab>e', window.split_top },
-
-    { '<tab>i', window.split_right },
-
-    ----------------------------------------------------------------------
-    --                              OTHER                               --
-    ----------------------------------------------------------------------
+----------------------------------------------------------------------
+--                              OTHER                               --
+----------------------------------------------------------------------
+wk.register({
     -- to go enable spell checker
-    { '<F6>', '<cmd>setlocal spell! spelllang=en_us<cr>' },
+    ['<F6>'] = { '<cmd>setlocal spell! spelllang=en_us<cr>', 'Toggle spell' },
 })
