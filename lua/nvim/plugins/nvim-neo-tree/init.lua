3fa2e2fb0b1409ba0f492e4d0f3cc850cb36a89a
s1n7ax <srineshnisala@gmail.com>
2022-12-09 23:11:33 +0530

    chore: using correct value to unmap key in neo-tree

--- lua/nvim/plugins/nvim-neo-tree/init.lua
+++ lua/nvim/plugins/nvim-neo-tree/init.lua
@@ -39,7 +39,7 @@ require('neo-tree').setup({
         window = {
             mappings = {
                 ['o'] = 'open',
-                ['e'] = ''
+                ['e'] = 'none'
             },
             position = 'current',
         },
