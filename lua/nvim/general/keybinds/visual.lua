06d4ce6789204512e1072c80f14d7ad97832a2c2
s1n7ax <srineshnisala@gmail.com>
2023-01-17 09:01:16 +0530

    feat: add mini.move to move lines in visual mode

--- lua/nvim/general/keybinds/visual.lua
+++
@@ -1,8 +0,0 @@
-local wk = require('which-key')
-
-wk.register({
-    ['<c-n>'] = { ":m '>+1<CR>gv=gv", 'Move up visual line' },
-    ['<c-e>'] = { ":m '<-2<CR>gv=gv", 'Move up visual line' },
-}, {
-    mode = 'v',
-})
