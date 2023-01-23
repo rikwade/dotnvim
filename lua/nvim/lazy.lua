62bb26e0ddcfb14db7c1f7fa72fce25be83447c1
s1n7ax <srineshnisala@gmail.com>
2023-01-23 22:42:51 +0530

    chore: code formatting

--- lua/nvim/lazy.lua
+++ lua/nvim/lazy.lua
@@ -1,53 +1,53 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        'git',
-        'clone',
-        '--filter=blob:none',
-        '--single-branch',
-        'git@github.com:folke/lazy.nvim.git',
-        lazypath,
-    })
+	vim.fn.system({
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'--single-branch',
+		'git@github.com:folke/lazy.nvim.git',
+		lazypath,
+	})
 end
 
 vim.opt.runtimepath:prepend(lazypath)
 
 require('lazy').setup('nvim.plugin', {
-    defaults = { lazy = true },
-    --  install = { colorscheme = { 'tokyonight', 'habamax' } },
-    checker = { enabled = true },
-    diff = {
-        cmd = 'terminal_git',
-    },
-    performance = {
-        cache = {
-            enabled = true,
-            -- disable_events = {},
-        },
-        rtp = {
-            disabled_plugins = {
-                'gzip',
-                'matchit',
-                'matchparen',
-                'netrwPlugin',
-                'tarPlugin',
-                'tohtml',
-                'tutor',
-                'zipPlugin',
-                'nvim-treesitter-textobjects',
-            },
-        },
-    },
-    ui = {
-        --  custom_keys = {
+	defaults = { lazy = true },
+	--  install = { colorscheme = { 'tokyonight', 'habamax' } },
+	checker = { enabled = true },
+	diff = {
+		cmd = 'terminal_git',
+	},
+	performance = {
+		cache = {
+			enabled = true,
+			-- disable_events = {},
+		},
+		rtp = {
+			disabled_plugins = {
+				'gzip',
+				'matchit',
+				'matchparen',
+				'netrwPlugin',
+				'tarPlugin',
+				'tohtml',
+				'tutor',
+				'zipPlugin',
+				'nvim-treesitter-textobjects',
+			},
+		},
+	},
+	ui = {
+		--  custom_keys = {
 
-            --  ['<localleader>d'] = function(plugin)
-                --  dd(plugin)
-            --  end,
-        --  },
-    },
-    debug = false,
+		--  ['<localleader>d'] = function(plugin)
+		--  dd(plugin)
+		--  end,
+		--  },
+	},
+	debug = false,
 })
 
 --  vim.keymap.set('n', '<leader>l', '<cmd>:Lazy<cr>')
