606a5d23e99c2eb8ca1d1088fc31532380677462
s1n7ax <srineshnisala@gmail.com>
2023-01-23 22:41:45 +0530

    chore: code split in browser util

--- lua/nvim/utils/nvim/browser/init.lua
+++ lua/nvim/utils/nvim/browser/init.lua
@@ -1,12 +1,18 @@
+local log = require('nvim.utils.lua.log')
+
 local M = {}
 
 function M.search_in_google()
 	local search_query = vim.fn.input('Query: ')
 
-	vim.fn.jobstart({
-		'chrome',
-		string.format('https://www.google.com/search?q=%s', search_query),
-	})
+	M.open(
+		string.format('https://www.google.com/search?q=%s', search_query)
+	)
+end
+
+function M.open(url)
+	log.info('opening url', url)
+	vim.fn.jobstart({ 'chrome', url })
 end
 
 return M
