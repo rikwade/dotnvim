9083431d6c325b6c9c8f666b0608a90ad060d4cb
s1n7ax <srineshnisala@gmail.com>
2023-02-05 13:53:15 +0530

    refactor: change dap ui keymap

--- lua/nvim/plugin/dap-ui/keymaps.lua
+++ lua/nvim/plugin/dap-ui/keymaps.lua
@@ -1,6 +1,22 @@
 local dapui = require('dapui')
 local wk = require('which-key')
 
-wk.register({
-	['<leader><leader>t'] = { dapui.toggle, 'Toggle dap ui' },
+local M = {}
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(arg)
+		local buffer = arg.buf
+		local server_name = vim.lsp.get_client_by_id(arg.data.client_id).name
+
+		M.on_attach(server_name, buffer)
+	end,
 })
+
+function M.on_attach(_, buffer)
+	wk.register({
+		name = 'Debug',
+		e = {
+			q = { dapui.toggle, 'Toggle dap ui' },
+		}
+	}, { prefix = '<leader>', buffer = buffer })
+end
