f160bb8011fdd95de325953c96341cb76b9c5827
s1n7ax <srineshnisala@gmail.com>
2023-03-05 09:09:30 +0530

    refactor: remove <c-t> map go normal mode

--- lua/nvim/general/keybinds/insert.lua
+++ lua/nvim/general/keybinds/insert.lua
@@ -3,7 +3,6 @@ local wk = require('which-key')
 wk.register({
 	['<c-a>'] = { '<esc>I', '(Insert) Jump to line start' },
 	['<c-o>'] = { '<esc>A', '(Insert) Jump to line end' },
-	['<c-t>'] = { '<esc>', 'Go to normal mode' },
 }, {
 	mode = 'i',
 })
