50e31b0041dd69f1128f09f1c905d5d3101cb2b0
s1n7ax <srineshnisala@gmail.com>
2023-03-09 10:09:37 +0530

    refactor: set which key timeout to 2 sec

--- lua/nvim/plugin/which-key/init.lua
+++ lua/nvim/plugin/which-key/init.lua
@@ -2,7 +2,11 @@ return {
 	'folke/which-key.nvim',
 	name = 'which-key',
 	version = '1.1.0',
-	config = {
-		triggers_blacklist = { i = { 'n' }, }
-	}
+	config = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 2000
+		require('which-key').setup({
+			triggers_blacklist = { i = { 'n' } },
+		})
+	end,
 }
