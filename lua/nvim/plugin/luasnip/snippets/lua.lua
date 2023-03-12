e98d1d2f18d448158fd9e3cfca39dd7f1fff3566
s1n7ax <srineshnisala@gmail.com>
2023-03-12 12:13:24 +0530

    feat: add return snip

--- lua/nvim/plugin/luasnip/snippets/lua.lua
+++ lua/nvim/plugin/luasnip/snippets/lua.lua
@@ -1,5 +1,6 @@
 local ls = require('luasnip')
 local lua = require('snips.lua')
+local common = require('snips.common')
 local s = ls.s
 local i = ls.insert_node
 
@@ -28,6 +29,7 @@ function M.setup()
 		s('fa', lua.primitives.noarg_anonymous_func()),
 		s('i', lua.primitives.import()),
 		s('o', lua.primitives.stdout()),
+		s('r', common.primitives.returns()),
 		s('oo', lua.primitives.pretty_print()),
 		s('td', lua.primitives.test_desc()),
 		s('ti', lua.primitives.test_it()),
--- lua/nvim/plugin/luasnip/snippets/rust.lua
+++ lua/nvim/plugin/luasnip/snippets/rust.lua
@@ -1,5 +1,6 @@
 local ls = require('luasnip')
 local rust = require('snips.rust')
+local common = require('snips.common')
 local s = ls.s
 
 local M = {}
@@ -8,6 +9,7 @@ function M.setup()
 	ls.add_snippets('rust', {
 		s('o', rust.dynamic.stdout()),
 		s('v', rust.primitives.variable()),
+		s('r', common.primitives.returns()),
 		s('vec', rust.primitives.vector_struct()),
 	})
 end
