00ede12e0ed1d96dd7c00b6c58a48344e2fe00e6
s1n7ax <srineshnisala@gmail.com>
2023-04-01 12:18:46 +0530

    fix: error cloning lazy.nvim in new os

--- lua/nvim/lazy.lua
+++ lua/nvim/lazy.lua
@@ -1,17 +1,17 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'--single-branch',
-		'git@github.com:folke/lazy.nvim.git',
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 
-vim.opt.runtimepath:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup('nvim.plugin', {
 	defaults = { lazy = true },
